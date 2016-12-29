715ce84d68612be9f74d145b9fa4c6e5bce42011
wmiya <1214087322@qq.com>
2016-12-29 23:35:48 +0800

    job

---
+++ 2016-12-8/data.js
@@ -0,0 +1,646 @@
+var data = {
+	l:{
+		pid:0,
+		li1:{
+			name:"第一级子菜单",
+			cont:"十",
+			list:{
+				pid:1,
+				li1:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li2:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li3:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		li2:{
+			name:"第一级子菜单",
+			cont:"十",
+			list:{
+				pid:1,
+				li1:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li2:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li3:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		li3:{
+			name:"第一级子菜单",
+			cont:"十",
+			list:{
+				pid:1,
+				li1:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li2:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li3:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	
+	},
+	r:{
+		pid:0,
+		li1:{
+			name:"第一级子菜单",
+			cont:"十",
+			list:{
+				pid:1,
+				li1:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li2:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li3:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		li2:{
+			name:"第一级子菜单",
+			cont:"十",
+			list:{
+				pid:1,
+				li1:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li2:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li3:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		li3:{
+			name:"第一级子菜单",
+			cont:"十",
+			list:{
+				pid:1,
+				li1:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li2:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				},
+				li3:{
+					name: "第二级子菜单",
+					list:{
+						pid:2,
+						li1:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li2:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						},
+						li3:{
+							name: "第三级子菜单",
+							list:{
+								pid:3,
+								li1:{
+									name:"第四级子菜单"
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	
+	}
+};
