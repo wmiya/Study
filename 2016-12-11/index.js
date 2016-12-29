6822424cb8bc4098c0ae98ab61b50d696eb2b9f3
wmiya <1214087322@qq.com>
2016-12-29 23:13:15 +0800

    job

---
+++ 2016-12-11/index.js
@@ -0,0 +1,26 @@
+
+
+function filesHtml(fileData){
+
+    	var tbInner = `
+    			<tr>
+		    		<td><input type="checkbox" id="checkAll"></td>
+		    		<td>1</td>
+		    		<td>大红</td>
+		    		<td>女</td>
+		    		<td>23</td>
+		    		<td>
+		    			<input type="button" value="上移"/>
+		    			<input type="button" value="下移" />
+		    			<input type="button" value="删除"/>
+		    		</td>
+			    </tr>
+    			`;
+    		
+    	return tbInner;
+	
+	
+	
+	
+	
+})()
